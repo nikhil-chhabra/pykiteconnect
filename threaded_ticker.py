291788ea19e4786a554b8dfb0c69680ecf1f00fc
nikhil-chhabra <57435652+nikhil-chhabra@users.noreply.github.com>
2024-04-12 12:53:38 +0530

    yyy

--- threaded_ticker.py
+++ threaded_ticker.py
@@ -18,13 +18,13 @@ from pymongo import MongoClient
 from jugaad_trader import Zerodha
 import pyotp
 import config
-
+import utils
 
 def market_hours():
     # Get current time
     current_time = datetime.now().time()
     # Define start and end times
-    start_time = time(9, 0)  # 9:00 AM
+    start_time = time(8, 45)  # 8:45 AM
     end_time = time(16, 0)   # 4:00 PM
     # Check if current time is between start and end times
     return start_time <= current_time <= end_time
@@ -35,18 +35,7 @@ password = config.password
 totp = pyotp.TOTP(config.totp_code)
 
 
-client = MongoClient()
-
-
-def insert_tick(tick):
-    db = client['kite']
-    collection = db['tick_data']
-    collection.insert_one(tick)
-
-
 logging.basicConfig(filename = 'log.txt', level=logging.INFO)
-
-
 
 kite = Zerodha(user_id=username, password=password, twofa=totp.now())
 
@@ -55,14 +44,12 @@ kite.login()
 kws = kite.ticker()
 
 try:
-    # Initialise.
-    date = datetime.now().strftime("%d_%m_%y")
-    df_instruments1 = pd.read_csv(f'files\weighted_final_shortlist_{date}.csv')
-    ce_shortlist_df = df_instruments1[(df_instruments1['signal_1d'] >= 0) & (df_instruments1['signal_7d'] > 0) & (df_instruments1['signal_31d'] > 0)]
-    pe_shortlist_df = df_instruments1[(df_instruments1['signal_1d'] <= 0) & (df_instruments1['signal_7d'] < 0) & (df_instruments1['signal_31d'] < 0)]
-
-    # RELIANCE BSE
-    tokens = ce_shortlist_df['instrument_token'].tolist()+pe_shortlist_df['instrument_token'].tolist()
+    scrips = utils.shortlist_scrips('5minute')
+    tokens = [i['instrument_token'] for i in scrips]
+    symbols = [i['tradingsymbol'] for i in scrips]
+    token_count=len(tokens)
+    dt=datetime.now()
+    logging.info(f"[{dt}] - {token_count} tokens found.")
 
 
     # Callback for tick reception.
@@ -71,24 +58,44 @@ try:
         if market_hours():
             if len(ticks) > 0:
                 ts=ticks[0]['exchange_timestamp']
-                logging.info(f"[{dt}] - Tick: {ts}")
-                for tick in ticks:
-                    insert_tick(tick)
+                if ts.year==datetime.now().year:
+                    logging.info(f"[{dt}] - Tick: {ts}")
+                    for tick in ticks:
+                        utils.insert_tick(tick)
+                else:
+                    message_close=f"[{dt}] - Market is closed due to holiday. Stopping the script"
+                    logging.info(message_close)
+                    utils.send_alert(message_close)
+                    ws.close()
+        else:
+            message_close=f"[{dt}] - Market is closed. Stopping the script"
+            logging.info(message_close)
+            utils.send_alert(message_close)
+            ws.close()
+            utils.sleep_computer()
 
 
     # Callback for successful connection.
     def on_connect(ws, response):
         dt=datetime.now()
-        logging.info(f"[{dt}] - Successfully connected. Response: {response}")
+        message_connect = f"[{dt}] - Successfully connected."
+        logging.info(message_connect)
+        utils.send_alert(message_connect)
         ws.subscribe(tokens)
         ws.set_mode(ws.MODE_FULL, tokens)
-        logging.info(f"[{dt}] - Subscribe to tokens in Full mode: {tokens}")
+        message_tokens = f"[{dt}] - Subscribe to tokens in Full mode: {symbols}"
+        logging.info(message_tokens)
+        utils.send_alert(message_tokens)
 
 
     # Callback when current connection is closed.
     def on_close(ws, code, reason):
         dt=datetime.now()
-        logging.info(f"[{dt}] - Connection closed: {code} - {reason}")
+        if code:
+            logging.info(f"[{dt}] - Connection closed: {code} - {reason}")
+        else:
+            logging.info(f"[{dt}] - Connection closed: Closure due to non market hours.")
+        ws.stop()
 
 
     # Callback when connection closed with error.
--- utils.py
+++ utils.py
@@ -1,14 +1,32 @@
-import concurrent.futures
 from datetime import datetime, timedelta
 import pandas as pd
 import plotly.graph_objects as go
 from datetime import datetime
 import os
+import time
 import config
 from jugaad_trader import Zerodha
 import pyotp
 from pymongo import MongoClient
 from tqdm import tqdm
+
+
+import requests
+import json
+
+def send_alert(message):
+    url = "https://api.telegram.org/bot7160202236:AAEXpCcvVl434EQEJK-iwlR5iUsaR5h3EUM/sendMessage"
+
+    payload = json.dumps({
+    "chat_id": "-1002114890478",
+    "text": str(message)
+    })
+    headers = {
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response
 
 def login_kite():
     username = config.username
@@ -73,14 +91,25 @@ def get_candle_signal(instrument_code, sample_size, interval, today=1):
         df_historical['candle_weight'] = df_historical['change'].abs() / df_historical['change'].abs().max()
         df_historical['candle_strength'] = df_historical['candle_type'] * df_historical['candle_weight']
         candle_ratio = sum(df_historical['candle_strength'])
-    return (candle_ratio / sample_size)
+        return (candle_ratio / sample_size)
+    else:
+        return 0
 
 
+def insert_tick(tick):
+    with MongoClient() as client:
+        db = client['kite']
+        collection = db['tick_data']
+        collection.insert_one(tick)
+
+def sleep_computer():
+    time.sleep(5)
+    os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
 
 def shortlist_scrips(interval):
     date = datetime.now().strftime("%d_%m_%y")
-    filename = f'files\weighted_final_shortlist_{date}.csv'
-    if filename not in os.listdir():
+    filename = f'files//weighted_final_shortlist_{date}.csv'
+    if os.path.basename(filename) not in os.listdir('files'):
         df_instruments1 = pd.read_excel('files//final_instruments.xlsx')
         for i in tqdm(range(len(df_instruments1))):
             df_instruments1.loc[i,'signal_1d']=get_candle_signal(df_instruments1.loc[i,'instrument_token'],1,interval)
@@ -89,9 +118,12 @@ def shortlist_scrips(interval):
         df_instruments1 = df_instruments1.sort_values(by="signal_31d", ascending=False).reset_index(drop=True)
         df_instruments1.to_csv(filename, index=False)
     df_instruments1 = pd.read_csv(filename)
-    buy_shortlist_df = df_instruments1[(df_instruments1['signal_1d'] >= 0) & (df_instruments1['signal_7d'] > 0) & (df_instruments1['signal_31d'] > 0)]
-    sell_shortlist_df = df_instruments1[(df_instruments1['signal_1d'] <= 0) & (df_instruments1['signal_7d'] < 0) & (df_instruments1['signal_31d'] < 0)]
-    return (buy_shortlist_df, sell_shortlist_df)
+    buy_shortlist_df = df_instruments1[(df_instruments1['signal_1d'] >= 0) & (df_instruments1['signal_7d'] > 0) & (df_instruments1['signal_31d'] > 0)].sort_values(by="signal_7d", ascending=False).head(20)
+    sell_shortlist_df = df_instruments1[(df_instruments1['signal_1d'] <= 0) & (df_instruments1['signal_7d'] < 0) & (df_instruments1['signal_31d'] < 0)].sort_values(by="signal_7d", ascending=True).head(20)
+    df_instruments_final=pd.concat([buy_shortlist_df,sell_shortlist_df])
+    df_instruments_final=df_instruments_final[['instrument_token','tradingsymbol']]
+    df_instruments_dict=df_instruments_final.to_dict(orient="records")
+    return df_instruments_dict
 
 
 def get_latest_tick(instrument_token):
